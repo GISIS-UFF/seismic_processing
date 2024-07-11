181f0a4c1fb2717375b31f7fde544f44d530eadb
Anthonyuff <anthonyrebello612@gmail.com>
2024-07-11 16:27:46 -0300

    New data

--- example.py
+++ example.py
@@ -2,7 +2,11 @@ from toolbox import managing as mng
 from toolbox import filtering as filter
 from toolbox import visualizing as view
 
-input_file = "data/teapot_dome_3D_survey.sgy"
+input_file = "data/overthrust_synthetic_seismic_data.sgy" # Synthetic data
+input_file2 = "data/seismic.segy" #Mobil AVO viking graben line 12
+input_file3 ="data/Line_001.sgy" #Poland 2D
+input_file4 ="data/npr3_field.sgy" #Teapot dome 3D survey (HEAVY FILE)
+
 
 data = mng.import_sgy_file(input_file)
 
@@ -10,20 +14,24 @@ mng.show_trace_header(data)
 
 key = 'src'
 
-index = 14
+indexes = view.keyword_indexes(data, key)
+
+print(indexes)
+
+index = 469
 
 view.gather(data, key, index)
 view.geometry(data, key, index)
-# view.fourier_fx_domain(data, key, index, fmin = 0, fmax = 100)
-# view.fourier_fk_domain(data, key, index, fmin = 0, fmax = 100)
+view.fourier_fx_domain(data, key, index, fmin = 0, fmax = 100)
+view.fourier_fk_domain(data, key, index, fmin = 0, fmax = 100)
 
-# fmin = 2    
-# fmax = 50
+fmin = 2    
+fmax = 50
 
-# output_file = f"data/overthrust_seismic_data_{fmin}-{fmax}Hz.sgy"
+output_file = f"data/overthrust_seismic_data_{fmin}-{fmax}Hz.sgy"
 
-# data_filt = filter.fourier_FX_domain(data, fmin, fmax, output_file)
+data_filt = filter.fourier_FX_domain(data, fmin, fmax, output_file)
 
-# view.fourier_fx_domain(data_filt, key, index, fmin = 0, fmax = 100)
+view.fourier_fx_domain(data_filt, key, index, fmin = 0, fmax = 100)
 
-# view.difference(data, data_filt, key, index)
+view.difference(data, data_filt, key, index)
