5f05b390b874c603c448fd8b58b0c33f84d7ca4f
Anthonyuff <anthonyrebello612@gmail.com>
2024-09-25 20:22:37 -0300

    adjusting

--- developers/anthony_analysis.py
+++ developers/anthony_analysis.py
@@ -4,6 +4,7 @@ path.append("../")
 from toolbox import managing as mng
 from toolbox import filtering as filter
 from toolbox import visualizing as view
+from toolbox import stacking as sta
 
 import numpy as np
 import matplotlib.pyplot as plt
@@ -23,145 +24,148 @@ print(f'numero de traço {data.tracecount}')
 mng.show_trace_header(data)
 
 print(data.trace[0])
-key = 'src'
+key = 'cmp'
 
-indexes = mng.keyword_indexes(data, key)
+indexes = mng.get_keyword_indexes(data, key)
 
 print(indexes)
 
-index = 304
+
+index = [304,200,500]
+sta.interactive_velocity_analysis(data,index)
 interpolated_line = None
 ponto_parada= None
-def inter(data, **kwargs) -> None:
-    fmax = kwargs.get("fmax") if "fmax" in kwargs else 100.0
-    key = kwargs.get("key") if "key" in kwargs else "src"
-    index = kwargs.get("index") if "index" in kwargs else mng.keyword_indexes(data, key)[0] 
 
-    mng.__check_keyword(key)
-    mng.__check_index(data, key, index)
+# def inter(data, **kwargs) -> None:
+    # fmax = kwargs.get("fmax") if "fmax" in kwargs else 100.0
+    # key = kwargs.get("key") if "key" in kwargs else "src"
+    # index = kwargs.get("index") if "index" in kwargs else mng.keyword_indexes(data, key)[0] 
 
-    byte = mng.__keywords.get(key)
+    # mng.__check_keyword(key)
+    # mng.__check_index(data, key, index)
 
-    traces = np.where(data.attributes(byte)[:] == index)[0]    
+    # byte = mng.__keywords.get(key)
 
-    nt = data.attributes(115)[0][0]
-    dt = data.attributes(117)[0][0] * 1e-6
+    # traces = np.where(data.attributes(byte)[:] == index)[0]    
 
-    seismic = data.trace.raw[:].T
-    seismic = seismic[:, traces]
+    # nt = data.attributes(115)[0][0]
+    # dt = data.attributes(117)[0][0] * 1e-6
 
-    frequency = np.fft.fftfreq(nt, dt)
-    fx_seismic = np.fft.fft(seismic, axis = 0)
+    # seismic = data.trace.raw[:].T
+    # seismic = seismic[:, traces]
 
-    for i in range(len(traces)):
-        fx_seismic[:, i] *= 1.0 / np.max(fx_seismic[:, i]) 
+    # frequency = np.fft.fftfreq(nt, dt)
+    # fx_seismic = np.fft.fft(seismic, axis = 0)
 
-    scale = 0.8 * np.std(seismic)
+    # for i in range(len(traces)):
+    #     fx_seismic[:, i] *= 1.0 / np.max(fx_seismic[:, i]) 
 
-    mask = np.logical_and(frequency >= 0.0, frequency <= fmax)
+    # scale = 0.8 * np.std(seismic)
 
-    floc = np.linspace(0, len(frequency[mask]) - 1, 11, dtype = int)
-    flab = np.around(np.ceil(frequency[floc]), decimals = 1)
+    # mask = np.logical_and(frequency >= 0.0, frequency <= fmax)
 
-    xloc = np.linspace(0, len(traces) - 1, 5, dtype = int)
-    xlab = traces[xloc]
+    # floc = np.linspace(0, len(frequency[mask]) - 1, 11, dtype = int)
+    # flab = np.around(np.ceil(frequency[floc]), decimals = 1)
 
-    fig, ax = plt.subplots(ncols = 1, nrows = 1, figsize = (10, 5))
-    points = []
+    # xloc = np.linspace(0, len(traces) - 1, 5, dtype = int)
+    # xlab = traces[xloc]
+
+    # fig, ax = plt.subplots(ncols = 1, nrows = 1, figsize = (10, 5))
+    # points = []
     
 
 
-    def onclick(event):
-        global ponto_parada
-        if event.inaxes is not None:
-            x, y = event.xdata, event.ydata
-            if ponto_parada is None:
-                points.append((x, y))
-                plt.plot(x, y, 'ro')
-                plt.draw()
-            else:
-                points.append((x, y))
-                plt.plot(x, y, 'bo')
-            plt.draw()
-        else:
-            print('Clique fora dos eixos.')
-    def stop(event):
-         global ponto_parada
-         if event.key == 'j':
+    # def onclick(event):
+    #     global ponto_parada
+    #     if event.inaxes is not None:
+    #         x, y = event.xdata, event.ydata
+    #         if ponto_parada is None:
+    #             points.append((x, y))
+    #             plt.plot(x, y, 'ro')
+    #             plt.draw()
+    #         else:
+    #             points.append((x, y))
+    #             plt.plot(x, y, 'bo')
+    #         plt.draw()
+    #     else:
+    #         print('Clique fora dos eixos.')
+    # def stop(event):
+    #      global ponto_parada
+    #      if event.key == 'j':
         
-            if ponto_parada is None:  
-                ponto_parada = (0, 0)
+    #         if ponto_parada is None:  
+    #             ponto_parada = (0, 0)
                 
-                plt.plot(ponto_parada[0], ponto_parada[1], 'go', label='Ponto de Parada')
-                plt.draw()
+    #             plt.plot(ponto_parada[0], ponto_parada[1], 'go', label='Ponto de Parada')
+    #             plt.draw()
 
-    def on_key(event):
-        if event.key=='n' and len(points) >= 1:
-            index = int(input('Escolha o ponto para deletar (começando de 0): '))
-            if 0 <= index < len(points):
-                points.pop(index)
-                ax.clear()
-                ax.imshow(np.abs(fx_seismic[mask, :]), aspect="auto", cmap="jet")
-                ax.set_yticks(floc)
-                ax.set_yticklabels(flab)
-                ax.set_xticks(xloc)
-                ax.set_xticklabels(xlab)
-                for p in points:
-                    plt.plot(p[0], p[1], 'ro')
-                    plt.draw()
+    # def on_key(event):
+    #     if event.key=='n' and len(points) >= 1:
+    #         index = int(input('Escolha o ponto para deletar (começando de 0): '))
+    #         if 0 <= index < len(points):
+    #             points.pop(index)
+    #             ax.clear()
+    #             ax.imshow(np.abs(fx_seismic[mask, :]), aspect="auto", cmap="jet")
+    #             ax.set_yticks(floc)
+    #             ax.set_yticklabels(flab)
+    #             ax.set_xticks(xloc)
+    #             ax.set_xticklabels(xlab)
+    #             for p in points:
+    #                 plt.plot(p[0], p[1], 'ro')
+    #                 plt.draw()
                 
 
    
 
-    def onkeypress(event):
-        global interpolated_line
-        if event.key == 'enter':
-            if len(points) > 1:
-                points_sorted = sorted(points, key = lambda p: p[0])
-                x, y = zip(*points_sorted)
+    # def onkeypress(event):
+    #     global interpolated_line
+    #     if event.key == 'enter':
+    #         if len(points) > 1:
+    #             points_sorted = sorted(points, key = lambda p: p[0])
+    #             x, y = zip(*points_sorted)
                 
 
-                interp_func = interp1d(x, y, kind = 'linear')
+    #             interp_func = interp1d(x, y, kind = 'linear')
 
-                x_new = np.linspace(min(x), max(x), num = 500)
-                y_new = interp_func(x_new)
-                if interpolated_line:
-                    interpolated_line.remove()
+    #             x_new = np.linspace(min(x), max(x), num = 500)
+    #             y_new = interp_func(x_new)
+    #             if interpolated_line:
+    #                 interpolated_line.remove()
 
-                interpolated_line, = plt.plot(x_new, y_new, 'b-')
-                plt.draw()
-        elif event.key =='m':
-            if interpolated_line:
-                interpolated_line.remove()  
-                interpolated_line = None  
-                plt.draw()
+    #             interpolated_line, = plt.plot(x_new, y_new, 'b-')
+    #             plt.draw()
+    #     elif event.key =='m':
+    #         if interpolated_line:
+    #             interpolated_line.remove()  
+    #             interpolated_line = None  
+    #             plt.draw()
 
 
-    fx = ax.imshow(np.abs(fx_seismic[mask, :]), aspect = "auto", cmap = "jet")
+    # fx = ax.imshow(np.abs(fx_seismic[mask, :]), aspect = "auto", cmap = "jet")
 
-    ax.set_yticks(floc)
-    ax.set_yticklabels(flab)
-    ax.set_xticks(xloc)
-    ax.set_xticklabels(xlab)
+    # ax.set_yticks(floc)
+    # ax.set_yticklabels(flab)
+    # ax.set_xticks(xloc)
+    # ax.set_xticklabels(xlab)
 
-    ax.set_ylabel("Frequency [Hz]", fontsize = 15)
-    ax.set_xlabel("Trace number", fontsize = 15)
+    # ax.set_ylabel("Frequency [Hz]", fontsize = 15)
+    # ax.set_xlabel("Trace number", fontsize = 15)
 
-    ax.cbar = fig.colorbar(fx, ax = ax)
-    ax.cbar.set_label("Amplitude", fontsize = 15) 
+    # ax.cbar = fig.colorbar(fx, ax = ax)
+    # ax.cbar.set_label("Amplitude", fontsize = 15) 
 
-    fig.tight_layout()
+    # fig.tight_layout()
 
-    fig.canvas.mpl_connect('button_press_event', onclick)
-    fig.canvas.mpl_connect('key_press_event', onkeypress)
-    fig.canvas.mpl_connect('key_press_event', on_key)
-    fig.canvas.mpl_connect('key_press_event', stop)
+    # fig.canvas.mpl_connect('button_press_event', onclick)
+    # fig.canvas.mpl_connect('key_press_event', onkeypress)
+    # fig.canvas.mpl_connect('key_press_event', on_key)
+    # fig.canvas.mpl_connect('key_press_event', stop)
 
-    plt.show()
+    # plt.show()
 
 #inter(data)
 
-view.semblance(data)
+# view.semblance(data)
 # P=np.loadtxt('coordernada',dtype = float)
 # print(len(P))
 # vint=np.zeros(len(P))
--- toolbox/stacking.py
+++ toolbox/stacking.py
@@ -10,6 +10,7 @@ from toolbox import filtering as filt
 
 ponto_parada= None
 interpolated_line = None
+points=[]
 
 def interactive_velocity_analysis(data : sgy.SegyFile, indexes : np.ndarray, **kwargs):
     """
@@ -32,6 +33,12 @@ def interactive_velocity_analysis(data : sgy.SegyFile, indexes : np.ndarray, **k
     ### Examples:
     
     >>> stk.interactive_velocity_analysis(data, indexes = cmps)
+
+    tutorial picking velocity
+    1)click on the map
+    2)after the points  placed press y and then enter
+    3) save is under maintenance
+    
     
     """
 
@@ -42,177 +49,178 @@ def interactive_velocity_analysis(data : sgy.SegyFile, indexes : np.ndarray, **k
     indexes = [indexes] if not isinstance(indexes, (np.ndarray, list)) else indexes 
 
     mng.__check_index(data, key, indexes)
+    for k in indexes:
+        print(k)
+        vmin = kwargs.get("vmin") if "vmin" in kwargs else 1000.0
+        vmax = kwargs.get("vmax") if "vmax" in kwargs else 3000.0 
+        nvel = kwargs.get("dv") if "dv" in kwargs else 101
 
-    vmin = kwargs.get("vmin") if "vmin" in kwargs else 1000.0
-    vmax = kwargs.get("vmax") if "vmax" in kwargs else 3000.0 
-    nvel = kwargs.get("dv") if "dv" in kwargs else 101
+        nt = data.attributes(115)[0][0]
+        dt = data.attributes(117)[0][0] * 1e-6
 
-    nt = data.attributes(115)[0][0]
-    dt = data.attributes(117)[0][0] * 1e-6
+        times = np.arange(nt) * dt
+        velocities = np.linspace(vmin, vmax, nvel)
 
-    times = np.arange(nt) * dt
-    velocities = np.linspace(vmin, vmax, nvel)
+        for index in [k]:
 
-    for index in indexes:
-
-        traces = np.where(data.attributes(byte)[:] == index)[0]  
-        
-        offsets = data.attributes(37)[traces] / data.attributes(69)[traces]
-        
-        seismic = np.zeros((nt, len(traces)))
-
-        gain = times ** 2
-
-        for i in range(len(traces)):
-            seismic[:,i] = data.trace.raw[traces[i]] * gain
-
-        domain = filt.radon_forward(seismic, dt, times, offsets, velocities, style = "hyperbolic")
-    
-        semblance = np.sum(np.abs(domain)**2, axis = 1)
-
-        semblance = gaussian_filter(semblance, sigma = 3.0)
-
-        semblance = np.abs(np.gradient(semblance, axis = 0))
-
-        semblance = gaussian_filter(semblance, sigma = 5.0)
-
-    # def onclick(event):
-        
-    #     global ponto_parada
-        
-    #     if event.inaxes is not None:
-        
-    #         x, y = event.xdata, event.ydata
-    #         if ponto_parada is None:
-    #             points.append((x, y))
-    #             plt.plot(x, y, 'ro')
-    #             plt.draw()
-                
-    #         else:
-    #             points.append((x, y))
-    #             plt.plot(x, y, 'bo')
-                
-    #         plt.draw()
-               
-    #     else:
-    #         print('Clique fora dos eixos.')
-
-
-    # def on_key(event):
-    #     if event.key=='n' and len(points) >= 1:
-    #         index = int(input('Escolha o ponto para deletar (começando de 0): '))
+            traces = np.where(data.attributes(byte)[:] == index)[0]  
             
-    #         if 0 <= index < len(points):
-    #             points.pop(index)
-    #             ax[1].clear()
-    #             im2 = ax[1].imshow(semblance, aspect='auto', cmap='jet', extent=[vmin, vmax, times[-1], times[0]])
-    #             ax[1].set_xlabel('Velocity [m/s]', fontsize=15)
-    #             ax[1].set_ylabel('Time [s]', fontsize=15)
-                
-    #             ax[1].set_xlim(vmin, vmax)
+            offsets = data.attributes(37)[traces] / data.attributes(69)[traces]
+            
+            seismic = np.zeros((nt, len(traces)))
 
-    #             ax[1].set_xlabel('Velocity [m/s]', fontsize=15)
-    #             ax[1].set_ylabel('Time [s]', fontsize=15)
-    
-    #             for p in points:
-    #                 plt.plot(p[0], p[1], 'ro')
-    #                 plt.draw()
-    
-    
-    # def stop(event):
-    #      global ponto_parada
-    #      if event.key == 'j':
+            gain = times ** 2
+
+            for i in range(len(traces)):
+                seismic[:,i] = data.trace.raw[traces[i]] * gain
+
+            domain = filt.radon_forward(seismic, dt, times, offsets, velocities, style = "hyperbolic")
         
-    #         if ponto_parada is None:  
-    #             ponto_parada = (0, 0)
+            semblance = np.sum(np.abs(domain)**2, axis = 1)
+
+            semblance = gaussian_filter(semblance, sigma = 3.0)
+
+            semblance = np.abs(np.gradient(semblance, axis = 0))
+
+            semblance = gaussian_filter(semblance, sigma = 5.0)
+
+        def onclick(event):
+            
+            global ponto_parada
+            
+            if event.inaxes is not None:
+            
+                x, y = event.xdata, event.ydata
+                if ponto_parada is None:
+                    points.append((x, y))
+                    plt.plot(x, y, 'ro')
+                    plt.draw()
+                    
+                else:
+                    points.append((x, y))
+                    plt.plot(x, y, 'bo')
+                    
+                plt.draw()
                 
-    #             plt.plot(ponto_parada[0], ponto_parada[1], 'go', label='Ponto de Parada')
-    #             plt.draw()    
-    
+            else:
+                print('Clique fora dos eixos.')
 
-    # def onkeypress(event):
-    
-    #     global interpolated_line
 
-    #     if event.key == 'enter':
-    #         if len(points) > 1:
-    #             points_sorted = sorted(points, key = lambda p: p[1])
-    #             x,y = zip(*points_sorted)
-               
-    #             cs = CubicSpline(y, x, bc_type='natural')
-    #             ynew = np.linspace(min(y), max(y), num = 500)
-    #             xnew = cs(ynew)
-    #             if interpolated_line:
-    #                 interpolated_line.remove()
-
-    #             interpolated_line, = plt.plot(xnew, ynew)
-    #             plt.draw()
-
-    #     elif event.key =='m':
-    #         if interpolated_line:
-    #             interpolated_line.remove()  
-    #             interpolated_line = None  
-    #             plt.draw()
-    
-    
-    # def save(event):
-    #     if event.key=='c':
-    #         np.savetxt("coordernada", points, fmt = "%.6f")
-    
-    
-    # def add(event):
-    #     if event.key=='y':
-    #             p0=points[0][0]
-    #             p1=points[-1][0]
-    #             points.append((p0,min(times),))
-    #             points.append((p1,max(times)))
-    #             print(points)
-    #             xn = [p[0] for p in points]
-    #             yn = [p[1] for p in points]
-    #             plt.plot(xn,yn,'ro')
-    #             plt.draw()
+        def on_key(event):
+            if event.key=='n' and len(points) >= 1:
+                index = int(input('Escolha o ponto para deletar (começando de 0): '))
                 
-    xloc = np.linspace(0, len(traces)-1, 5, dtype = int)
+                if 0 <= index < len(points):
+                    points.pop(index)
+                    ax[1].clear()
+                    im2 = ax[1].imshow(semblance, aspect='auto', cmap='jet', extent=[vmin, vmax, times[-1], times[0]])
+                    ax[1].set_xlabel('Velocity [m/s]', fontsize=15)
+                    ax[1].set_ylabel('Time [s]', fontsize=15)
+                    
+                    ax[1].set_xlim(vmin, vmax)
 
-    tloc = np.linspace(0, nt-1, 11, dtype = int)
-    tlab = np.around(tloc * dt, decimals = 3)
-    
-    vloc = np.linspace(0, nvel-1, 5, dtype = int)
-    vlab = velocities[vloc]
+                    ax[1].set_xlabel('Velocity [m/s]', fontsize=15)
+                    ax[1].set_ylabel('Time [s]', fontsize=15)
+        
+                    for p in points:
+                        plt.plot(p[0], p[1], 'ro')
+                        plt.draw()
+        
+        
+        #def stop(event):
+            #  global ponto_parada
+            #  if event.key == 'j':
+            
+            #     if ponto_parada is None:  
+            #         ponto_parada = (0, 0)
+                    
+            #         plt.plot(ponto_parada[0], ponto_parada[1], 'go', label='Ponto de Parada')
+            #         plt.draw()    
+        
 
-    fig, ax = plt.subplots(ncols = 2, nrows = 1, figsize = (10, 5)) 
+        def onkeypress(event):
+        
+            global interpolated_line
 
-    im1 = ax[0].imshow(seismic, aspect = 'auto', cmap = 'Greys')
-    ax[0].set_xticks(xloc)
-    ax[0].set_yticks(tloc)
-    ax[0].set_xticklabels(xloc)
-    ax[0].set_yticklabels(tlab)
-    ax[0].set_xlabel('Relative trace index', fontsize = 15) 
-    ax[0].set_ylabel('Time [s]', fontsize = 15)
-    cbar1 = fig.colorbar(im1, ax = ax[0])
-    cbar1.set_label("Amplitude", fontsize = 15)
+            if event.key == 'enter':
+                if len(points) > 1:
+                    points_sorted = sorted(points, key = lambda p: p[1])
+                    x,y = zip(*points_sorted)
+                
+                    cs = CubicSpline(y, x, bc_type='natural')
+                    ynew = np.linspace(min(y), max(y), num = 500)
+                    xnew = cs(ynew)
+                    if interpolated_line:
+                        interpolated_line.remove()
 
-    im2 = ax[1].imshow(semblance, aspect = 'auto', cmap = 'jet')
-    ax[1].set_xlabel('Velocity [m/s]', fontsize = 15) 
-    ax[1].set_ylabel('Time [s]', fontsize = 15)
+                    interpolated_line, = plt.plot(xnew, ynew)
+                    plt.draw()
 
-    ax[1].grid()
-    ax[1].set_xticks(vloc)
-    ax[1].set_yticks(tloc)
-    ax[1].set_xticklabels(vlab)
-    ax[1].set_yticklabels(tlab)
-    ax[1].set_xlabel('Velocity [m/s]', fontsize = 15) 
-    ax[1].set_ylabel('Time [s]', fontsize = 15)
-    cbar2 = fig.colorbar(im2, ax = ax[1])
-    cbar2.set_label("Amplitude", fontsize = 10)
-    
-    fig.tight_layout()
-    # fig.canvas.mpl_connect('button_press_event', onclick)
-    # fig.canvas.mpl_connect('key_press_event', onkeypress)
-    # fig.canvas.mpl_connect('key_press_event', on_key)
-    # fig.canvas.mpl_connect('key_press_event', stop)
-    # fig.canvas.mpl_connect('key_press_event', save)
-    # fig.canvas.mpl_connect('key_press_event', add)  
-    plt.show()
+            elif event.key =='m':
+                if interpolated_line:
+                    interpolated_line.remove()  
+                    interpolated_line = None  
+                    plt.draw()
+        
+        
+        def save(event):
+            if event.key=='c':
+                np.savetxt("coordernada", points, fmt = "%.6f")
+        
+        
+        def add(event):
+            if event.key=='y':
+                    p0=points[0][0]
+                    p1=points[-1][0]
+                    points.append((p0,min(times),))
+                    points.append((p1,max(times)))
+                    print(points)
+                    xn = [p[0] for p in points]
+                    yn = [p[1] for p in points]
+                    plt.plot(xn,yn,'ro')
+                    plt.draw()
+                    
+        xloc = np.linspace(0, len(traces)-1, 5, dtype = int)
+
+        tloc = np.linspace(0, nt-1, 11, dtype = int)
+        tlab = np.around(tloc * dt, decimals = 3)
+        
+        vloc = np.linspace(0, nvel-1, 5, dtype = int)
+        vlab = velocities[vloc]
+
+        fig, ax = plt.subplots(ncols = 2, nrows = 1, figsize = (10, 5)) 
+
+        im1 = ax[0].imshow(seismic, aspect = 'auto', cmap = 'Greys')
+        ax[0].set_xticks(xloc)
+        ax[0].set_yticks(tloc)
+        ax[0].set_xticklabels(xloc)
+        ax[0].set_yticklabels(tlab)
+        ax[0].set_xlabel('Relative trace index', fontsize = 15) 
+        ax[0].set_ylabel('Time [s]', fontsize = 15)
+        cbar1 = fig.colorbar(im1, ax = ax[0])
+        cbar1.set_label("Amplitude", fontsize = 15)
+
+        im2 = ax[1].imshow(semblance, aspect = 'auto', cmap = 'jet')
+        ax[1].set_xlabel('Velocity [m/s]', fontsize = 15) 
+        ax[1].set_ylabel('Time [s]', fontsize = 15)
+
+        ax[1].grid()
+        ax[1].set_xticks(vloc)
+        ax[1].set_yticks(tloc)
+        ax[1].set_xticklabels(vlab)
+        ax[1].set_yticklabels(tlab)
+        ax[1].set_xlabel('Velocity [m/s]', fontsize = 15) 
+        ax[1].set_ylabel('Time [s]', fontsize = 15)
+        cbar2 = fig.colorbar(im2, ax = ax[1])
+        cbar2.set_label("Amplitude", fontsize = 10)
+        
+        fig.tight_layout()
+        fig.canvas.mpl_connect('button_press_event', onclick)
+        fig.canvas.mpl_connect('key_press_event', onkeypress)
+        fig.canvas.mpl_connect('key_press_event', on_key)
+        fig.canvas.mpl_connect('key_press_event', stop)
+        fig.canvas.mpl_connect('key_press_event', save)
+        fig.canvas.mpl_connect('key_press_event', add)  
+        plt.show()
     
 
