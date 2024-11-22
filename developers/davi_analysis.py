6b519626712b879226dbfd30df379aa9a325a5e6
davi.melonio <davi.melonio@eliis.fr>
2024-11-22 18:49:56 -0300

    small changes

--- developers/davi_analysis.py
+++ developers/davi_analysis.py
@@ -9,15 +9,16 @@ from toolbox import managing as mng
 PATH = "../data/overthrust_synthetic_seismic_data" 
 
 data = mng.import_sgy_file(PATH)
+# print(mng.get_keyword_indexes(data, 'off'))
 
-key = "rec"
-index = 120 
+key = "off"
+index = 7500 
 
 seismic, tickNLabels = load_seismic(data, key, index)
 seismic2 = np.copy(seismic)
 
 dt = data.attributes(117)[0][0] * 1e-6
-agc_operator = 10 # [ms]
+agc_operator = 50 # [ms]
 sliding_window = int(agc_operator / (1e3 * dt))
 
 nt, traces = seismic.shape
@@ -33,14 +34,13 @@ for i in range(traces):
 
         l, h, mid = l + 1, h + 1, mid + 1
 
+scale = 0.9 * np.std(seismic2)
 fig, ax = plt.subplots(nrows=1, ncols=2, figsize=(10, 8))
 
-scale = 0.9*np.std(seismic)
-im = ax[0].imshow(seismic, aspect = "auto", cmap = "Greys", vmin = -scale, vmax = scale)
+im = ax[0].imshow(seismic, aspect="auto", cmap="Greys", vmin=-scale, vmax=scale)
 ax[0].set_title("Normal Seismic")
 
-scale2 = 0.9*np.std(seismic2)
-im2 = ax[1].imshow(seismic2, aspect="auto", cmap="Greys", vmin=-scale2, vmax=scale2)
+im2 = ax[1].imshow(seismic2, aspect="auto", cmap="Greys", vmin=-scale, vmax=scale)
 ax[1].set_title("AGC Seismic")
 
 plt.show()
@@ -54,23 +54,7 @@ ax[0].set_title("Normal Trace")
 ax[1].plot(seismic2[:, target_trace])
 ax[1].set_title("AGC Trace")
 
+for i in ax:
+    i.set_ylim((min(seismic2[:, target_trace]), max(seismic2[:, target_trace])))
+    
 plt.show()
-
-def apply_agc(seismic: np.array, agc_operator: int, dt: float):
-    sliding_window = int(agc_operator / (1e3 * dt))
-
-    nt, traces = seismic.shape
-
-    for i in range(traces):
-        trace = seismic2[:, i]
-        l, h = 0, sliding_window - 1
-        mid = (l + h) // 2
-        while h < nt - 1:
-            window_samples = trace[l:h]
-            mean_amplitude = np.mean(np.abs(window_samples))
-            trace[mid] /= mean_amplitude + 1e-6
-
-            l, h, mid = l + 1, h + 1, mid + 1
-
-    return data
-
