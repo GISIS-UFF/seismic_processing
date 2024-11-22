beb4c0b0089b642752835c1d9a572b213b2ca4cd
davi.melonio <davi.melonio@eliis.fr>
2024-11-22 18:06:01 -0300

    add agc function

--- developers/davi_analysis.py
+++ developers/davi_analysis.py
@@ -33,7 +33,7 @@ for i in range(traces):
 
         l, h, mid = l + 1, h + 1, mid + 1
 
-fig, ax = plt.subplots(nrows=1, ncols=3, figsize=(10, 8))
+fig, ax = plt.subplots(nrows=1, ncols=2, figsize=(10, 8))
 
 scale = 0.9*np.std(seismic)
 im = ax[0].imshow(seismic, aspect = "auto", cmap = "Greys", vmin = -scale, vmax = scale)
@@ -42,11 +42,6 @@ ax[0].set_title("Normal Seismic")
 scale2 = 0.9*np.std(seismic2)
 im2 = ax[1].imshow(seismic2, aspect="auto", cmap="Greys", vmin=-scale2, vmax=scale2)
 ax[1].set_title("AGC Seismic")
-
-diff = seismic - seismic2
-scale3 = 0.9*np.std(diff)
-im3 = ax[2].imshow(diff, aspect="auto", cmap="Greys", vmin=-scale3, vmax=scale3)
-ax[2].set_title("Diff A - B")
 
 plt.show()
 
@@ -59,7 +54,23 @@ ax[0].set_title("Normal Trace")
 ax[1].plot(seismic2[:, target_trace])
 ax[1].set_title("AGC Trace")
 
-for i in ax:
-    i.set_ylim((min(seismic2[:, target_trace]), max(seismic2[:, target_trace])))
-    
 plt.show()
+
+def apply_agc(seismic: np.array, agc_operator: int, dt: float):
+    sliding_window = int(agc_operator / (1e3 * dt))
+
+    nt, traces = seismic.shape
+
+    for i in range(traces):
+        trace = seismic2[:, i]
+        l, h = 0, sliding_window - 1
+        mid = (l + h) // 2
+        while h < nt - 1:
+            window_samples = trace[l:h]
+            mean_amplitude = np.mean(np.abs(window_samples))
+            trace[mid] /= mean_amplitude + 1e-6
+
+            l, h, mid = l + 1, h + 1, mid + 1
+
+    return data
+
